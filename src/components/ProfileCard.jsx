25133c4f95a5c51380ce5e348a371a5a46d1988e
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-12 11:00:00 +0000

    enhancement: changed profile card styling

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,10 +4,13 @@ import "./ProfileCard.css";
 
 const ProfileCard = () => {
   return (
-    <div className="userCard">
+    <div className="userCard element">
       <div className="gradiant"></div>
       <div className="profileDown">
-        {/* <img src={profile_icon} alt="" /> */}
+        <img
+          src="https://images.unsplash.com/photo-1488161628813-04466f872be2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hbnxlbnwwfHwwfHx8Mg%3D%3D"
+          alt=""
+        />
         <div className="profileTitle">Tim James</div>
         <div className="profileDescription">
           Insert dummy user information here
