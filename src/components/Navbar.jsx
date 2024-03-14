fbdd59ad911747749ad1434fbf85b079bf7f6964
Ethan Rose <ethanrose.er@googlemail.com>
2024-03-14 14:56:07 +0000

    enhancement: changed logo import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { userContext } from "../common/contexts";
 import NightModeToggle from "./NightModeToggle";
 import { GiHamburgerMenu } from "react-icons/gi";
 import LogoutButton from "./LogoutButton";
+import logo from "../assets/weathercode.png";
 
 export const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +24,7 @@ export const Navbar = (props) => {
       <div className={`navbar-menu ${!isOpen && "hide-mobile"}`}>
         {user && (
           <>
-            <img className="navbar-logo" src="public/weathercode.png" />
+            <img className="navbar-logo" src={logo} />
             <div className="navbar-links">
               <NavLink to="/">
                 <button className="user-info-button">Dashboard</button>
